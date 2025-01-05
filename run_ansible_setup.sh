1f8e514fe82de2bd2a79348073fa713deb4b4760
G37SYS73M <g37sys73m@localhost.localdomain>
2025-01-05 21:16:40 +0530

    Edited

--- run_ansible_setup.sh
+++ run_ansible_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 # Set variables
 ANSIBLE_PLAYBOOK="ansible/mythic.yml" # Path to the Ansible playbook
 INVENTORY_FILE="ansible/inventory.ini" # Path to the Ansible inventory file
@@ -14,11 +15,10 @@ echo "Ansible completed successfully! The setup is done."
 
 
 
-echo 'Use Command: ssh -i .ssh/id_rsa -F 7443:127.0.0.1:7443 ubuntu@$ip'
-echo "to get the mythic_admin password"
-echo "/opt/Mythic"
+echo '[*]Use Command: ssh -i .ssh/id_rsa -L 7443:127.0.0.1:7443 ubuntu@$ip'
+echo "[*]To get the mythic_admin password"
+echo "cd /opt/Mythic"
 echo "sudo ./mythic-cli config get MYTHIC_ADMIN_PASSWORD"
-
 Echo "Visit https://127.0.0.1:7443"
 
 
